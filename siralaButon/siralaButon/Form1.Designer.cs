24f2f888d9fa21ff2b51a8fb5dce6e9ff53e27c6
Aziz Mert Zengin <121314749+azizmertzengin@users.noreply.github.com>
2022-12-25 12:46:28 +0300

    Buton Sıralama Oyunu

---
+++ siralaButon/siralaButon/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace siralaButon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1108, 305);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(135, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(728, 412);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1104, 200);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SÜRE: 60 SN";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(875, 27);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 412);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1230, 493);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ siralaButon/siralaButon/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace siralaButon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Button> butonlar = new List<Button>();
+        Random rnd = new Random();
+        int[] sayilar = new int[9];
+        int saniye = 60;
+        int sayac = 0;
+        bool checker = false;
+        private void gameOver()
+        {
+            sayac = 0;
+            timer1.Stop();
+            checker = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!checker)
+            {
+                listBox1.Items.Clear();
+                checker = true;
+                for (int i = 0; i < 9; i++)
+                {
+                    sayilar[i] = rnd.Next(0, 100);
+                    Button btn = new Button();
+                    btn.Text = sayilar[i].ToString();
+                    btn.Name = "buton" + i.ToString();
+                    btn.SetBounds(groupBox1.Width - rnd.Next(0, groupBox1.Width - 75), groupBox1.Height - rnd.Next(0, groupBox1.Height - 75), 50, 50);
+                    btn.ForeColor = Color.Black;
+                    btn.BackColor = Color.Lime;
+                    this.Controls.Add(btn);
+                    butonlar.Add(btn);
+                    btn.Click += new EventHandler(Btn_Click);
+                }
+                groupBox1.SendToBack();
+                Array.Sort(sayilar);
+                saniye = 60;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Oyun zaten devam ediyor.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);
+            if (Convert.ToInt32(btn.Text) == sayilar[sayac])
+            {
+                listBox1.Items.Add(btn.Text);
+                btn.Visible = false;
+                sayac++;
+                if (sayac == 9)
+                {
+                    gameOver();
+                    MessageBox.Show(saniye.ToString() + " Sn kala oyunu bitirdin.", Name = "Tebriks");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Daha küçük sayılar var. Dikkatli ol oyun tuzaklı olabilir!");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye--;
+            label1.Text = "SÜRE: " + saniye.ToString() + " SN";
+            if (saniye <= 0)
+            {
+                //Butonları gizleme fonksiyonu eklenmesi gerek**
+                gameOver();
+                MessageBox.Show("Süre bitti. Maalesef bölümü geçemedin.", Name = ":(");
+            }
+        }
+    }
+}
